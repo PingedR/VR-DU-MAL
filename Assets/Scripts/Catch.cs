6a591008496e9a2c70e8a613997178f5386b26cf
PingedR <reugenio82@hotmail.com>
2023-03-04 16:12:59 -0300

    commit inicial mesmo

---
+++ Assets/Scripts/Catch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Family"))
+        {
+            //collision.gameObject.SetActive(false);
+            Debug.Log("Family");
+        }
+   
+    } 
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject Bullet;
+    public float speed;
+    private Rigidbody rig_bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+           GameObject createdBullet = Instantiate(Bullet);
+
+           createdBullet.transform.position = transform.position;
+
+           rig_bullet = createdBullet.GetComponent<Rigidbody>();
+
+           Camera cam = Camera.main;
+
+           rig_bullet.velocity = cam.transform.rotation * Vector3.forward * speed;
+
+        }
+    }
+}
