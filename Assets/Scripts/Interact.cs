b4e632905807328064d01a4b627c9dcb37eafd3a
PingedR <reugenio82@hotmail.com>
2023-03-18 16:07:54 -0300

    Interact script

---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    private MeshRenderer malha;
+    private Color corori;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        malha = GetComponent<MeshRenderer>();
+        corori = malha.material.color;
+    }
+
+    public void Selected()
+    {
+        malha.material.color = Color.white;
+    }
+
+    public void NotSelected()
+    {
+        malha.material.color = corori;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
