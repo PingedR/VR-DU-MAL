ab9be2912e12b5fc30a63950939871fbc32a6786
PingedR <reugenio82@hotmail.com>
2023-04-01 16:08:31 -0300

    Spawner e AI

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Interact : MonoBehaviour
 {
     private MeshRenderer malha;
     private Color corori;
+    public Image circleProgress;
+    public float totalTime = 3;
+    private bool gvrStatus;
+    private float gvrTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -14,19 +19,28 @@ public class Interact : MonoBehaviour
         corori = malha.material.color;
     }
 
+    void Update()
+    {
+        if (gvrStatus == true)
+        {
+            gvrTimer += Time.deltaTime;
+            circleProgress.fillAmount = gvrTimer / totalTime;
+        }
+        else
+        {
+            gvrTimer = 0;
+            circleProgress.fillAmount = 0;
+        }
+    }
     public void Selected()
     {
         malha.material.color = Color.white;
+        gvrStatus = true;
     }
 
     public void NotSelected()
     {
         malha.material.color = corori;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        gvrStatus = false;
     }
 }
---
+++ Assets/Scripts/Touch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : MonoBehaviour
+{
+    public GameObject Scary;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Scary.SetActive(true);
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Scary.SetActive(false);
+        }
+    }
+}
